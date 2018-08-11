edfef412be69d57f6f7db514503a46d42652513d
Letmesea <1101913493@qq.com>
2018-08-11 10:53:11 +0800

    app.py

---
+++ python_webapp/www/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author : Letmesea
+# @Time : 2018/8/11 10:47
+# @Email :  : LIANGZHONGHAI3@ceair.com
+# @File : app.py
+# @TODO :
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>')
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
